f09fb78e0cffa0eb8d8a94b521fffc056eacb386
Aditya Vikram <adityavikram54@gmail.com>
2024-05-02 18:48:37 -0400

    Added threadpool implementation

---
+++ README.md
@@ -0,0 +1,50 @@
+# Simple Thread Pool Library
+
+The Simple Thread Pool Library is a lightweight header-only C++ library that provides a simple and efficient way to manage and execute tasks in parallel using a thread pool.
+
+## Getting Started
+To use the Simple Thread Pool Library, simply include the `SimpleThreadPool.hpp` header file in your project.
+
+```cpp
+#include "SimpleThreadPool.hpp"
+```
+
+## Example
+The following example demonstrates how to use the Simple Thread Pool Library to calculate the sum of the first 1000 natural numbers in parallel.
+
+```cpp
+#include <iostream>
+#include "SimpleThreadPool.hpp"
+
+int main() {
+    // Create a thread pool with 4 threads
+    SimpleThreadPool threadPool(4);
+
+    // Define a task that calculates the sum of integers in the range [start, end) modulo (1e9 + 7)
+    constexpr auto sumTask = [](std::uint64_t start, std::uint64_t end, std::atomic<int>& result) {
+        int sum = 0;
+        for (std::uint64_t i = start; i < end; ++i) {
+            sum = (sum + i) % 1000000007;
+        }
+        result = (result + sum) % 1000000007;
+    };
+
+    // Queue 100 tasks to calculate the sum of integers in the range [0, 1e9) in parallel
+    std::atomic<int> result(0);
+    for (std::uint64_t i = 0; i < 100; ++i) {
+        threadPool.queue([i, &sumTask, &result] { sumTask(i * 10000000, (i + 1) * 10000000, result); });
+    }
+
+    // Wait for all tasks to complete
+    threadPool.waitForTasks();
+    std::cout << "Sum of integers in the range [0, 1e9) modulo (1e9 + 7) = " << result << std::endl;
+
+    // Stop processing tasks: no more tasks will be queued, but the threads will continue processing the remaining tasks
+    // threadPool.stopProcessing();
+
+    return 0;
+}
+```
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ SimpleThreadPool.hpp
@@ -0,0 +1,86 @@
+#ifndef SIMPLE_THREAD_POOL_H
+#define SIMPLE_THREAD_POOL_H
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <condition_variable>
+
+class SimpleThreadPool {
+public:
+    using Task = std::function<void()>;
+
+    SimpleThreadPool(uint8_t num_threads) {
+        threads.reserve(num_threads);
+        for (uint8_t i = 0; i < num_threads; ++i) {
+            threads.push_back(std::thread([this] { threadHandler(); }));
+        }
+    }
+
+    void queue(const Task& task) {
+        {
+            std::lock_guard<std::mutex> taskQueueLock(taskQueueMutex);
+            taskQueue.push(task);
+        }
+        stopPoolCV.notify_one();
+    }
+
+    void stopProcessing() {
+        {
+            std::lock_guard<std::mutex> taskQueueLock(taskQueueMutex);
+            shouldStop = true;
+        }
+        stopPoolCV.notify_all();
+    }
+
+    void waitForTasks() {
+        std::unique_lock<std::mutex> taskQueueLock(taskQueueMutex);
+        tasksWaitCV.wait(taskQueueLock, [this] { return taskQueue.empty() && tasksInProgress == 0; });
+    }
+
+    SimpleThreadPool(const SimpleThreadPool&) = delete;
+    SimpleThreadPool& operator=(const SimpleThreadPool&) = delete;
+
+    ~SimpleThreadPool() noexcept {
+        stopProcessing();
+        for (auto& thread: threads) {
+            thread.join();
+        }
+    }
+private:
+    std::vector<std::thread> threads;
+
+    std::queue<Task> taskQueue;
+    std::mutex taskQueueMutex;
+
+    std::condition_variable stopPoolCV;
+    std::condition_variable tasksWaitCV;
+    bool shouldStop = false;
+    unsigned int tasksInProgress = 0;
+
+    void threadHandler() {
+        while (true) {
+            std::unique_lock<std::mutex> taskQueueLock(taskQueueMutex);
+            stopPoolCV.wait(taskQueueLock, [this] { return !taskQueue.empty() || shouldStop; });
+            if (!taskQueue.empty()) {
+                Task task = taskQueue.front();
+                taskQueue.pop();
+                ++tasksInProgress;
+                taskQueueLock.unlock();
+
+                task();
+
+                taskQueueLock.lock();
+                --tasksInProgress;
+                tasksWaitCV.notify_one();
+            } else if (shouldStop) {
+                break;
+            }
+        }
+    }
+};
+
+
+#endif //SIMPLE_THREAD_POOL_H
